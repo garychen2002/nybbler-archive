0ffb28b559e027c814d5cf56e6f574dcd6b597f3
Ian Gregory <justcheesy@outlook.com>
2024-07-03 00:13:56 -0400

    server: run all Ghidra analysis scripts

--- server/helpers/analyze.ts
+++ server/helpers/analyze.ts
@@ -10,9 +10,10 @@ const output_projectname = "testproject"; // use the random id of the uploaded f
 // watch out for user defined names passing into arguments
 const input_filename = path.join("c:\\", "temp", "ascii.txt"); // pass in from uploads
 const scripts_path = path.join(__dirname, "..", "scripts");
-const output_symbol_filename = "symbols.json";
-const output_instructions_filename = "";
-const output_decomp_filename = "";
+const output_symbol_filename = ".symbols.json";
+const output_instructions_filename = ".instructions.json";
+const output_codeunits_filename = ".codeunits.json";
+const output_decomp_filename = ".decomp.cpp";
 
 // credit to https://ben.page/node-spawn-callback for reference on making a callback with spawn
 export const analyze_ghidra = (file_to_analyze: string) =>
@@ -23,6 +24,9 @@ export const analyze_ghidra = (file_to_analyze: string) =>
       '-import', file_to_analyze,
       "-scriptPath", scripts_path,
       "-postScript", "ExportSymbolInfoScript.java", file_to_analyze + output_symbol_filename,
+      "-postScript", "InstructionDisassembler.java", file_to_analyze + output_instructions_filename,
+      "-postScript", "CodeUnitDisassembler.java", file_to_analyze + output_codeunits_filename,
+      "-postScript", "DecompilerOutput.java", file_to_analyze + output_decomp_filename,
       // add more postScripts for each script here
       "-deleteProject"
     ];
