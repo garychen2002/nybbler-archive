c352fe380a0886f822f6c5e6f210da18b745ddcd
Ian Gregory <justcheesy@outlook.com>
2024-07-10 18:04:26 -0400

    client: integrate automerge at app level

--- client/src/models/project.ts
+++ client/src/models/project.ts
@@ -1,9 +1,11 @@
+import type { DocumentId } from '@automerge/automerge-repo'
 import type { Binary } from './binaries/binary'
 import type { User } from './user'
 
 export type Project = {
   id: number
   name: string
+  automergeDocumentId: DocumentId
   invitees: User[]
   binaries: Binary[]
 }
--- client/src/models/project_metadata.ts
+++ client/src/models/project_metadata.ts
@@ -3,5 +3,6 @@ import type { User } from './user'
 export type ProjectMetadata = {
   id: number
   name: string
+  automergeDocumentId: string
   invitees: User[]
 }
