8aa591a145da8476d0cdc982e074003fc20b0730
Ian Gregory <justcheesy@outlook.com>
2024-07-27 13:38:24 -0400

    client: include authorization token in export download requests

--- client/src/components/ExportButton.vue
+++ client/src/components/ExportButton.vue
@@ -1,22 +1,43 @@
 <script setup lang="ts">
 import { apiProjects } from '@/services/api'
-import { VaButton } from 'vuestic-ui'
+import { useToast, VaButton } from 'vuestic-ui'
 
 const props = defineProps<{
   projectId: string
 }>()
 
-function downloadFile(url: string) {
+function downloadFile(url: string, filename?: string) {
   const a = document.createElement('a')
   a.href = url
+  if (filename) a.download = filename
   a.click()
 }
 
-const { VITE_API_BASE_URL } = import.meta.env
+const toast = useToast()
 
 async function exportProject() {
-  downloadFile(VITE_API_BASE_URL + `/api/projects/${props.projectId}/exported`)
-  await apiProjects.get(`${props.projectId}/exported`)
+  const toastID = toast.init({ message: `exporting project…`, duration: -1 })
+
+  // https://stackoverflow.com/a/43133108
+  try {
+    const res = await apiProjects.get(`${props.projectId}/exported`, { responseAs: 'response' })
+    const blob = await res.blob()
+
+    downloadFile(
+      URL.createObjectURL(blob),
+      res.headers.get('Content-Disposition')?.match(/attachment;\s*filename="([^"]+)"/i)?.[1]
+    )
+
+    toast.notify({ message: `project exported.`, color: 'success' })
+  } catch (error) {
+    toast.notify({
+      message: `failed to export project.`,
+      color: 'danger'
+    })
+    console.error(error)
+  } finally {
+    toast.close(toastID!)
+  }
 }
 </script>
 
