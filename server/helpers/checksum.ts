300a205376a5bca51c4e32e7297086c26161a056
Gary Chen <thegary.chen@mail.utoronto.ca>
2024-07-27 03:50:10 -0400

    run prettier

--- server/helpers/checksum.ts
+++ server/helpers/checksum.ts
@@ -2,21 +2,20 @@
 import { createHash } from "crypto";
 import { createReadStream } from "fs";
 export const getChecksum = async (path: string): Promise<string> => {
-    return new Promise(function (resolve, reject) {
-      // crypto.createHash('sha1');
-      // crypto.createHash('sha256');
-      const hash = createHash('md5');
-      const input = createReadStream(path);
-  
-      input.on('error', reject);
-  
-      input.on('data', function (chunk) {
-        hash.update(chunk);
-      });
-  
-      input.on('close', function () {
-        resolve(hash.digest('hex'));
-      });
+  return new Promise(function (resolve, reject) {
+    // crypto.createHash('sha1');
+    // crypto.createHash('sha256');
+    const hash = createHash("md5");
+    const input = createReadStream(path);
+
+    input.on("error", reject);
+
+    input.on("data", function (chunk) {
+      hash.update(chunk);
     });
-  }
-  
+
+    input.on("close", function () {
+      resolve(hash.digest("hex"));
+    });
+  });
+};
