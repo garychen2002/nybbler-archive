b15de3d31156fae6ff4fa85da4b56bdd5ab90637
Gary Chen <thegary.chen@mail.utoronto.ca>
2024-07-28 20:18:32 -0400

    add function router authorization

--- server/src/routers/function_router.ts
+++ server/src/routers/function_router.ts
@@ -1,12 +1,31 @@
 import { Router } from "express";
 import { Function } from "../models/function.js";
-import { catchErrors } from "../shared.js";
+import { catchErrors, getAuthenticatedUser, STATUS_FORBIDDEN } from "../shared.js";
+import { Invite } from "../models/invite.js";
 
 export const functionRouter = Router();
 
 functionRouter.get(
   "/:functionId",
   catchErrors(async (req, res) => {
+    const user = await getAuthenticatedUser(req);
+    const funct = await Function.findOne({
+      where: {
+        id: req.params.functionId,
+      }
+    })
+    if (funct?.symbol?.binary?.project)
+    {
+      const projectInvites = await Invite.findOne({
+        where: {
+          projectId: funct?.symbol?.binary?.projectId,
+          userId: user?.id,
+        },
+      });
+      if (!projectInvites) {
+        res.status(STATUS_FORBIDDEN).send();
+      }
+    }
     res.json(await Function.findByPk(Number(req.params.functionId)));
   }),
 );
